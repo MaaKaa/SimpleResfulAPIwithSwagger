291c22ebfc25ca2adc7cadfdbfe6e5749e0d6b43
Marzena Kaa <kacprowicz.marzena@gmail.com>
2020-03-19 19:08:58 +0100

    Create Book model, BooksController and SwaggerConfig.

---
+++ src/main/java/pl/maakaa/app/SimpleRestfulApiWithSwaggerApplication.java
@@ -0,0 +1,13 @@
+package pl.maakaa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestfulApiWithSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestfulApiWithSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maakaa/app/SimpleRestfulApiWithSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maakaa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestfulApiWithSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
